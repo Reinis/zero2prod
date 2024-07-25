3e611db1e4e989cbca719fa6b8d4c4bfa28ef8a1
Reinis Danne <rei4dan@gmail.com>
2024-07-25 23:20:18 +0300

    test: Use LazyLock

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,23 @@
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use std::sync::OnceLock;
+use std::sync::LazyLock;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-static TRACING: OnceLock<()> = OnceLock::new();
+static TRACING: LazyLock<()> = LazyLock::new(|| {
+    let default_filter_level = "info".into();
+    let subscriber_name = "test".into();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub url: String,
@@ -14,19 +25,7 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
-    TRACING.get_or_init(|| {
-        let default_filter_level = "info".into();
-        let subscriber_name = "test".into();
-
-        if std::env::var("TEST_LOG").is_ok() {
-            let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
-            init_subscriber(subscriber);
-        } else {
-            let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
-            init_subscriber(subscriber);
-        }
-    });
-
+    let _ = TRACING;
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let url = format!("http://127.0.0.1:{}", port);
@@ -37,6 +36,7 @@ async fn spawn_app() -> TestApp {
     let connection_pool = configure_database(&configuration.database).await;
 
     let server = run(listener, connection_pool.clone()).expect("Failed to bind adddress");
+    #[allow(clippy::let_underscore_future)]
     let _ = tokio::spawn(server);
     TestApp {
         url,
