488db5066a719de1368b49e613f1f62d66689fdc
Reinis Danne <rei4dan@gmail.com>
2024-10-18 23:45:48 +0300

    scripts: init_db: Add docker network

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,17 +3,19 @@ set -x
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
-	echo >&2 "Error: psql is not installed"
-	exit 1
+  echo >&2 "Error: psql is not installed"
+  exit 1
 fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
-	echo >&2 "Error: sqlx is not installed"
-	echo >&2 "Use:"
-	echo >&2 "    cargo install sqlx-cli --no-default-features --features rustls,postgres"
-	echo >&2 "to install it."
-	exit 1
+  echo >&2 "Error: sqlx is not installed"
+  echo >&2 "Use:"
+  echo >&2 "    cargo install sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
 fi
+
+docker network inspect zero2prod || docker network create --driver bridge zero2prod
 
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
@@ -22,21 +24,23 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 if [[ -z ${SKIP_DOCKER} ]]; then
-	docker run \
-		-e POSTGRES_USER=${DB_USER} \
-		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
-		-e POSTGRES_DB=${DB_NAME} \
-		-p "${DB_PORT}":5432 \
-		-d postgres \
-		postgres -N 1000
-	# ^ Increased maximum number of connections for testing purposes
+  docker run \
+    --name zero2prod_postgres \
+    --network zero2prod \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+  # ^ Increased maximum number of connections for testing purposes
 fi
 
 # Keep pinging postgres until it is ready
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-	>&2 echo "Postgres is still unavailable — sleeping"
-	sleep 1
+  >&2 echo "Postgres is still unavailable — sleeping"
+  sleep 1
 done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
